4db25177d840f0c4d17b782d857abd83ddb36ae0
John Ky <newhoggy@gmail.com>
2016-06-21 20:56:26 +1000

    Add documentation

--- pico-event/src/main/scala/org/pico/event/Var.scala
+++ pico-event/src/main/scala/org/pico/event/Var.scala
@@ -4,17 +4,62 @@ import java.util.concurrent.atomic.AtomicReference
 
 import org.pico.atomic.syntax.std.atomicReference._
 
+/** An atomic mutable variable that can act as an event source.
+  *
+  * It always has a current value.
+  *
+  * @tparam A The type of the value the mutable variable stores
+  */
 trait Var[A] extends Live[A] {
+  /** Assign a new value to the variable
+    *
+    * The new value will be emitted.
+    *
+    * @param newValue New new value
+    * @return The value that was assigned
+    */
   def value_=(newValue: A): A
 
+  /** Atomically get and set a value to the variable.
+    *
+    * The new value will be emitted.
+    *
+    * @param a The new value
+    * @return The value that was replaced
+    */
   def getAndSet(a: A): A
 
-  def compareAndSet(expect: A, update: A): Boolean
+  /** Atomically set the replacement value to the variable provided that the current value is
+    * equal to the expected value.
+    *
+    * The new value will be emitted if the set was successful.
+    *
+    * @param expect The expected current value
+    * @param replacement The replacement value
+    * @return true if the replacement value was set
+    */
+  def compareAndSet(expect: A, replacement: A): Boolean
 
+  /** Atomically update the existing value with the result of its application to the provided
+    * function.  This method will retry until the value is set successfully.
+    *
+    * The new value will be emitted.
+    *
+    * @param f The transforming function.  Because of retries, this function should be inexpensive
+    *          and must not have any side-effects.
+    * @return The replaced value.
+    */
   def update(f: A => A): A
 }
 
 object Var {
+  /** Create an atomic mutable variable with an initial value that can act as an event source that
+    * emits each newly assigned value.
+    *
+    * @param initial The initial value
+    * @tparam A The type of the initial value
+    * @return The atomic mutable variable
+    */
   def apply[A](initial: A): Var[A] = {
     val state = new AtomicReference[A](initial)
 
--- pico-event/src/main/scala/org/pico/event/syntax/source/package.scala
+++ pico-event/src/main/scala/org/pico/event/syntax/source/package.scala
@@ -5,10 +5,23 @@ import org.pico.event.{Live, SimpleBus, Sink, Source}
 
 package object source {
   implicit class SourceOps_hJob2ex[A](val self: Source[A]) extends AnyVal {
+    /** Create a live value with an initial value, which will have the latest value that was
+      * emitted by the event source.
+      *
+      * @param initial The initial value
+      * @return The live value that will change to contain the latest value emitted by the source
+      */
     def latest(initial: A): Live[A] = self.foldRight(initial)((v, _) => v)
   }
 
   implicit class SourceOps_KhVNHpu[A, B](val self: Source[Either[A, B]]) extends AnyVal {
+    /** Divert values on the left of emitted events by the source into the provided sink.
+      *
+      * Values on the right of the event will be emitted by the returned source.
+      *
+      * @param sink The sink to which left side of emitted events will be published
+      * @return The source that emits the right side of emitted events
+      */
     def divertLeft(sink: Sink[A]): Source[B] = {
       new SimpleBus[B] { temp =>
         temp += self.subscribe {
@@ -18,6 +31,13 @@ package object source {
       }
     }
 
+    /** Divert values on the right of emitted events by the source into the provided sink.
+      *
+      * Values on the left of the event will be emitted by the returned source.
+      *
+      * @param sink The sink to which right side of emitted events will be published
+      * @return The source that emits the left side of emitted events
+      */
     def divertRight(sink: Sink[B]): Source[A] = {
       new SimpleBus[A] { temp =>
         temp += self.subscribe {
